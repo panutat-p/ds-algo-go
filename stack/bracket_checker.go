81acb95b4660f94805d65c2725cac5aa54efdb68
panutat-p <pp.panutat@gmail.com>
2022-01-10 15:42:46 +0700

    stack, refactor bracket_checker.go

--- stack/bracket_checker.go
+++ stack/bracket_checker.go
@@ -1,24 +1,23 @@
 package stack
 
 import (
-	"container/list"
 	"fmt"
 )
 
-// use stack to collect left bracket and pop to test right bracket
-
+// IsBalancedBrackets
+// when found left bracket, look for right bracket and store to stack
 func IsBalancedBrackets(str string) bool {
 	leftBrackets := map[string]string{"(": ")", "[": "]", "{": "}", "<": ">"}
 	rightBrackets := map[string]bool{")": true, "]": true, "}": true, ">": true}
 
-	s := Stack{list.New()}
+	s := New()
 	for _, v := range str {
 		b := string(v)
-		if right, ok := leftBrackets[b]; ok {
+		if right, ok := leftBrackets[b]; ok { // it is left bracket
 			s.Push(right)
 			s.Print()
-		} else if rightBrackets[b] {
-			if s.IsEmpty() || s.Pop() != b { // test right bracket
+		} else if rightBrackets[b] { // it is right bracket
+			if s.IsEmpty() || s.Pop() != b {
 				fmt.Println("found mismatch char:", b)
 				return false
 			}
