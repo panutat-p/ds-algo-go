df70bd9e94b26225ca8a5f58a114d21bab0fc665
panutat-p <pp.panutat@gmail.com>
2022-01-12 21:22:05 +0700

    implement circular queue

---
+++ queue/circular_queue/circular_queue.go
@@ -0,0 +1,116 @@
+package circular_queue
+
+import (
+	"errors"
+	"fmt"
+)
+
+/*
+https://www.programiz.com/dsa/circular-queue
+*/
+
+type CircularQueue struct {
+	sl         []interface{}
+	frontIndex int // first element
+	rearIndex  int // behind the last element
+	isWrapped  bool
+}
+
+func New() *CircularQueue {
+	sl := make([]interface{}, 4)
+	return &CircularQueue{sl, 0, 0, false}
+}
+
+func (cq CircularQueue) Peek() interface{} {
+	return cq.sl[cq.frontIndex]
+}
+
+func (cq *CircularQueue) Enqueue(v interface{}) {
+	// in case of no Dequeue() call
+	// we cannot wrap the slice to replace the value at first index
+	// so, we resize the slice
+	if cq.Size() == len(cq.sl)-1 { // 🦄 we have to resize the array when there's only one empty position at the last index
+		cq.resizeSlice()
+	}
+
+	cq.sl[cq.rearIndex] = v
+	if cq.rearIndex == len(cq.sl)-1 { // reach at the last element
+		cq.rearIndex = 0 // 🦄 wrap
+		cq.isWrapped = true
+	} else {
+		cq.rearIndex += 1 // right shift to empty element
+	}
+}
+
+func (cq *CircularQueue) Dequeue() (interface{}, error) {
+	if cq.Size() == 0 {
+		return nil, errors.New("cannot dequeue, queue is empty")
+	}
+
+	rslt := cq.sl[cq.frontIndex]
+	cq.sl[cq.frontIndex] = nil
+	cq.frontIndex += 1
+
+	// 🦊 handle boundary cases
+	if cq.Size() == 0 { // reset queue 👍🏻
+		// this happens when frontIndex hit the rearIndex
+		cq.frontIndex = 0
+		cq.rearIndex = 0
+	} else if cq.isWrapped && cq.frontIndex == len(cq.sl) {
+		// this can happen when slice was wrapped, if not, frontIndex will hit rearIndex before hit the length
+		cq.frontIndex = 0
+	}
+
+	return rslt, nil
+}
+
+func (cq CircularQueue) Size() int {
+	if cq.frontIndex <= cq.rearIndex {
+		return cq.rearIndex - cq.frontIndex
+	} else { // 🦄 already wrapped
+		return len(cq.sl) + (cq.rearIndex - cq.frontIndex) // total length - number of empty elements
+	}
+}
+
+// resize and unwrap the slice if slice was wrapped
+// [4,5,1,2,3] >> [1,2,3,4,5,...]
+//    ^ ^          ^       ^
+func (cq *CircularQueue) resizeSlice() {
+	sl := make([]interface{}, 2*len(cq.sl))
+	if cq.isWrapped {
+		index := 0
+		for i := cq.frontIndex; i < len(cq.sl); i += 1 {
+			sl[index] = cq.sl[i]
+			index += 1
+		}
+		for i := 0; i < cq.rearIndex; i += 1 {
+			sl[index] = cq.sl[i]
+			index += 1
+		}
+		cq.sl = sl
+		cq.isWrapped = false
+		cq.frontIndex = 0
+		cq.rearIndex = index
+	} else {
+		for i, v := range cq.sl {
+			sl[i] = v
+		}
+		cq.sl = sl
+	}
+}
+
+func (cq CircularQueue) Print() {
+	if cq.isWrapped {
+		for i := cq.frontIndex; i < len(cq.sl); i += 1 {
+			fmt.Printf("%v -> ", cq.sl[i])
+		}
+		for i := 0; i < cq.rearIndex; i += 1 {
+			fmt.Printf("%v -> ", cq.sl[i])
+		}
+	} else {
+		for i := 0; i < cq.rearIndex; i += 1 {
+			fmt.Printf("%v -> ", cq.sl[i])
+		}
+	}
+	fmt.Println()
+}
---
+++ queue/circular_queue/circular_queue_test.go
@@ -0,0 +1,78 @@
+package circular_queue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCircularQueue_Size(t *testing.T) {
+	cq := New()
+	for i := 0; i < 10; i += 1 {
+		fmt.Printf("%+v\n", cq)
+		fmt.Println("Size:", cq.Size())
+		cq.Enqueue(i)
+	}
+	fmt.Println(cq.Dequeue())
+	fmt.Println(cq.Dequeue())
+	fmt.Printf("%+v\n", cq)
+	fmt.Println("Size:", cq.Size())
+
+	cq.Print()
+
+	if cq.Size() != 8 {
+		t.Errorf("got %v, but expect %v", cq.Size(), 8)
+	}
+}
+
+func TestCircularQueue_reSizeSlice(t *testing.T) {
+	cq := New()
+	for i := 0; i < 6; i += 1 {
+		cq.Enqueue(i)
+	}
+	fmt.Printf("%+v\n", cq)
+	fmt.Println("Size:", cq.Size())
+	cq.Dequeue()
+	cq.Dequeue()
+	cq.Dequeue()
+	cq.Dequeue()
+	fmt.Printf("%+v\n", cq)
+	fmt.Println("Size:", cq.Size())
+	for i := 6; i < 13; i += 1 {
+		cq.Enqueue(i)
+		fmt.Printf("%+v\n", cq)
+		fmt.Println("Size:", cq.Size())
+	}
+
+	cq.Print()
+
+	if cq.Size() != 9 {
+		t.Errorf("got %v, but expect %v", cq.Size(), 9)
+	}
+}
+
+func TestCircularQueue_Dequeue(t *testing.T) {
+	cq := New()
+	for i := 0; i < 6; i += 1 {
+		cq.Enqueue(i)
+	}
+	cq.Dequeue()
+	cq.Dequeue()
+	cq.Dequeue()
+	cq.Dequeue()
+	for i := 6; i < 10; i += 1 {
+		cq.Enqueue(i)
+	}
+	fmt.Printf("%+v\n", cq)
+	fmt.Println("Size:", cq.Size())
+	cq.Dequeue()
+	cq.Dequeue()
+	cq.Dequeue()
+	fmt.Printf("%+v\n", cq)
+	fmt.Println("Size:", cq.Size())
+
+	cq.Print()
+
+	if cq.Size() != 3 {
+		t.Errorf("got %v, but expect %v", cq.Size(), 3)
+	}
+}
