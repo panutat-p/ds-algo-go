788dd5c2bd1263b84770eae4e25ef8707d5b21e8
panutat-p <pp.panutat@gmail.com>
2023-04-12 00:21:05 +0700

    feat: quick sort

--- main.go
+++ main.go
@@ -3,17 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{2, 5, 1, 4}
-	selectionSort(nums)
-	fmt.Println("🟩 want [1 2 4 5]", "got", nums)
+	fmt.Println("🟩 want [1 2 4 5]", "got", quickSort([]int{2, 5, 1, 4}))
 }
 
-func selectionSort(nums []int) {
-	for i := 0; i < len(nums); i += 1 {
-		for j := i + 1; j < len(nums); j += 1 {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
-			}
+func quickSort(nums []int) []int {
+	if len(nums) == 0 {
+		return []int{}
+	}
+
+	if len(nums) == 1 {
+		return nums
+	}
+
+	pivot := nums[0]
+	var (
+		left  []int
+		right []int
+	)
+	for _, v := range nums[1:] {
+		if pivot > v {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
 		}
 	}
+
+	return append(append(quickSort(left), pivot), quickSort(right)...)
 }
