3ee8f0b2bc78481e7cf206dd4356c1d5db9793f5
panutat-p <pp.panutat@gmail.com>
2023-05-11 22:42:45 +0700

    chore: stack

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{5, 3, 1, 9, 4, 8, 6, 2, 7}
-	selectionSort(nums)
-	fmt.Println("🟩", nums)
+	s := NewStack()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
 }
 
-func selectionSort(nums []int) {
-	for i := 0; i < len(nums); i += 1 {
-		for j := i + 1; j < len(nums); j += 1 {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
-			}
-		}
-	}
+type Stack []int
+
+func NewStack() Stack {
+	return Stack{}
+}
+
+func (s *Stack) Peek() int {
+	return (*s)[len(*s)-1]
+}
+
+func (s *Stack) Push(e int) {
+	*s = append(*s, e)
+}
+
+func (s *Stack) Pop() int {
+	e := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return e
 }
