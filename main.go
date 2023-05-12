b41750003e237d2180c1e57f8b60968cc1504181
panutat-p <pp.panutat@gmail.com>
2023-05-13 04:05:44 +0700

    chore: selection sort

--- main.go
+++ main.go
@@ -3,31 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	s := NewStack()
-	s.Push(1)
-	s.Push(2)
-	s.Push(3)
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
-	fmt.Println(s.Pop())
+	sl := []int{5, 4, 9, 6, 1, 2}
+	selectionSort(sl)
+	fmt.Println(sl)
 }
 
-type Stack []int
-
-func NewStack() Stack {
-	return Stack{}
-}
-
-func (s *Stack) Peek() int {
-	return (*s)[len(*s)-1]
-}
-
-func (s *Stack) Push(e int) {
-	*s = append(*s, e)
-}
-
-func (s *Stack) Pop() int {
-	e := (*s)[len(*s)-1]
-	*s = (*s)[:len(*s)-1]
-	return e
+func selectionSort(nums []int) {
+	for i := 0; i < len(nums); i += 1 {
+		for j := i + 1; j < len(nums); j += 1 {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
 }
