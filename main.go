5335c2cabc29f7477531f1c42922f133e3f96ca2
panutat-p <pp.panutat@gmail.com>
2023-05-13 23:55:40 +0700

    chore: quick sort

--- main.go
+++ main.go
@@ -4,16 +4,32 @@ import "fmt"
 
 func main() {
 	sl := []int{5, 4, 9, 6, 1, 2}
-	selectionSort(sl)
+	sl = quickSort(sl)
 	fmt.Println(sl)
 }
 
-func selectionSort(nums []int) {
-	for i := 0; i < len(nums); i += 1 {
-		for j := i + 1; j < len(nums); j += 1 {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
-			}
+func quickSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	var (
+		left  []int
+		right []int
+		pivot = nums[0]
+	)
+	for _, e := range nums[1:] {
+		if pivot > e {
+			left = append(left, e)
+		} else {
+			right = append(right, e)
 		}
 	}
+
+	var (
+		ret []int
+	)
+	ret = append(quickSort(left), pivot)
+	ret = append(ret, quickSort(right)...)
+	return ret
 }
