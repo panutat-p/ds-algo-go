20ecde24034034126266c3da4cbc69a2b11921b2
panutat.p <pp.panutat@gmail.com>
2023-10-29 19:36:20 +0700

    refactor: plain bubble sort

--- sorts/bubble_sort/back_to_front.go
+++ sorts/bubble_sort/back_to_front.go
@@ -1,7 +1,7 @@
 package bubble_sort
 
 func BackToFront(nums []int) []int {
-	for i := 0; i < len(nums)-1; i += 1 {
+	for i := 0; i < len(nums); i += 1 {
 		for j := 0; j < len(nums)-1; j += 1 {
 			if nums[j] > nums[j+1] {
 				nums[j], nums[j+1] = nums[j+1], nums[j]
