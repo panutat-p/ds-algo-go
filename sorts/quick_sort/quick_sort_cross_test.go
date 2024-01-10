2f3b22fae5ef741c2c9e7ce1a83c5d6c0fd6458c
panutat.p <pp.panutat@gmail.com>
2024-01-11 01:28:50 +0700

    test: quick sort

--- sorts/quick_sort/quick_sort_cross_test.go
+++ sorts/quick_sort/quick_sort_cross_test.go
@@ -2,34 +2,26 @@ package quick_sort
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
 
-func IsSorted(sl []int) bool {
-	for i := 0; i < len(sl)-1; i += 1 {
-		if sl[i] > sl[i+1] {
-			return false
-		}
-	}
-	return true
-}
-
 func TestQuickSortCross(t *testing.T) {
-	sl := []int{30, 5, 6, 27, 100, 3, 51, 7, 49, 0, 4, 63}
-	QuickSortCross(sl, 0, len(sl)) // ⚠️ pass len index
-	fmt.Println(sl)
+	t.Run("12 elements", func(t *testing.T) {
+		sl := []int{30, 5, 6, 27, 100, 3, 51, 7, 49, 0, 4, 63}
+		QuickSortCross(sl, 0, len(sl))
+		fmt.Println(sl)
+		if !slices.IsSorted(sl) {
+			t.Error("\nslice is not sorted\ngot", sl)
+		}
+	})
 
-	if !IsSorted(sl) {
-		t.Errorf("got %v, but expect %v", IsSorted(sl), true)
-	}
-}
-
-func TestQuickSortCross2(t *testing.T) {
-	sl := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	QuickSortCross(sl, 0, len(sl)) // ⚠️ pass len index
-	fmt.Println(sl)
-
-	if !IsSorted(sl) {
-		t.Errorf("got %v, but expect %v", IsSorted(sl), true)
-	}
+	t.Run("sorted input", func(t *testing.T) {
+		sl := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		QuickSortCross(sl, 0, len(sl))
+		fmt.Println(sl)
+		if !slices.IsSorted(sl) {
+			t.Error("\nslice is not sorted\ngot", sl)
+		}
+	})
 }
--- sorts/quick_sort/quick_sort_swap_test.go
+++ sorts/quick_sort/quick_sort_swap_test.go
@@ -2,6 +2,7 @@ package quick_sort
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
 
@@ -9,9 +10,8 @@ func TestQuickSortSwap(t *testing.T) {
 	sl := []int{30, 5, 6, 27, 100, 3, 51, 7, 49, 0, 4, 63}
 	QuickSortSwap(sl, 0, len(sl)-1) // ⚠️ pass last index
 	fmt.Println(sl)
-
-	if !IsSorted(sl) {
-		t.Errorf("got %v, but expect %v", IsSorted(sl), true)
+	if !slices.IsSorted(sl) {
+		t.Error("\nslice is not sorted\ngot", sl)
 	}
 }
 
@@ -19,8 +19,7 @@ func TestQuickSortSwap2(t *testing.T) {
 	sl := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	QuickSortSwap(sl, 0, len(sl)-1) // ⚠️ pass last index
 	fmt.Println(sl)
-
-	if !IsSorted(sl) {
-		t.Errorf("got %v, but expect %v", IsSorted(sl), true)
+	if !slices.IsSorted(sl) {
+		t.Error("\nslice is not sorted\ngot", sl)
 	}
 }
