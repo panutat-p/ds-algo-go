8c9e1dd56fbfc0fc1c90954fb1f37fedcb220488
panutat.p <pp.panutat@gmail.com>
2024-01-14 01:37:25 +0700

    refactor: main

--- main.go
+++ main.go
@@ -2,34 +2,50 @@ package main
 
 import "fmt"
 
+var count int
+
 func main() {
-	sl := []int{5, 4, 9, 6, 1, 2}
-	sl = quickSort(sl)
+	sl := []int{7, 5, 4, 13, 9, 10, 54, -4, 3, 8, 6, 1, 2}
+	sl = sort(sl)
+	fmt.Println(count)
 	fmt.Println(sl)
 }
 
-func quickSort(nums []int) []int {
-	if len(nums) <= 1 {
+func sort(nums []int) []int {
+	// edge
+	if len(nums) < 2 {
 		return nums
 	}
 
+	// pivot
 	var (
 		left  []int
 		right []int
-		pivot = nums[0]
 	)
-	for _, e := range nums[1:] {
-		if pivot > e {
-			left = append(left, e)
+	pivot := nums[0]
+	for _, n := range nums[1:] {
+		if pivot > n {
+			left = append(left, n)
 		} else {
-			right = append(right, e)
+			right = append(right, n)
 		}
+		count += 1
 	}
 
-	var (
-		ret []int
-	)
-	ret = append(quickSort(left), pivot)
-	ret = append(ret, quickSort(right)...)
-	return ret
+	// repeat
+	left = sort(left)
+	right = sort(right)
+
+	// join
+	var ret []int
+	ret = append(ret, left...)
+	ret = append(ret, pivot)
+	ret = append(ret, right...)
+	return nums
 }
+
+// bubble 156
+// bubble optimized 78
+// selection 78
+// quick 34
+// merge ?
