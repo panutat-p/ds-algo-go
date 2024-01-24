489ad8c30d005a74e36b139a1eba9e86f2b5f509
panutat.p <pp.panutat@gmail.com>
2024-01-25 00:07:43 +0700

    test: merge sort

--- sorts/merge_sort/merge_sort_slice_test.go
+++ sorts/merge_sort/merge_sort_slice_test.go
@@ -6,25 +6,42 @@ import (
 )
 
 func TestMergeSortSlice(t *testing.T) {
-	t.Run("10 elements", func(t *testing.T) {
-		given := []int{34, 3, 4, 9, 0, 5, 65, 5, 41, 7}
-		got := MergeSortSlice(given)
-		if !slices.IsSorted(got) {
-			t.Errorf("\nGot %v\nWant sorted slice", got)
-		}
-	})
-	t.Run("positives", func(t *testing.T) {
-		given := []int{7, 3, 5, 4, 9, 1}
-		got := MergeSortSlice(given)
-		if !slices.IsSorted(got) {
-			t.Errorf("\nGot %v\nWant sorted slice", got)
-		}
-	})
-	t.Run("negatives", func(t *testing.T) {
-		given := []int{-6, -5, -4, -11, -2}
-		got := MergeSortSlice(given)
-		if !slices.IsSorted(got) {
-			t.Errorf("\nGot %v\nWant sorted slice", got)
-		}
-	})
+	tests := []struct {
+		name  string
+		given []int
+	}{
+		{
+			name:  "1 to 5",
+			given: []int{4, 1, 3, 5, 1, 2},
+		},
+		{
+			name:  "positive",
+			given: []int{30, 5, 6, 27, 100, 3, 51, 7, 49, 0, 4, 63},
+		},
+		{
+			name:  "negative",
+			given: []int{14, 4, 26, -3, 0, 1, -34},
+		},
+		{
+			name:  "pure negative",
+			given: []int{-6, -5, -4, -11, -2},
+		},
+		{
+			name:  "10 elements",
+			given: []int{34, 3, 4, 9, 0, 5, 65, 5, 41, 7},
+		},
+		{
+			name:  "12 elements",
+			given: []int{112, 12, 46, 3, 68, -3, 0, 1, 4, -49, 14, 26},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := MergeSortSlice(tt.given)
+			if !slices.IsSorted(got) {
+				t.Error("\nWant", "sorted slice", "\nGot ", got)
+			}
+		})
+	}
 }
